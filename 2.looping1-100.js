71954b2eae20544ffac2933527c54ea424f7fc05
ariiyantto168 <ariiyantto168@gmail.com>
2020-06-10 18:32:37 +0700

    updated

--- 2.looping1-100.js
+++ 2.looping1-100.js
@@ -1,7 +1,6 @@
 var laundry;
 i = 1;
 for (laundry=1; laundry<=100; laundry++){
-    // console.log(laundry + ". masukkan pakaian ke mesin cuci");
     if (laundry % 3) {
         console.log('fizz')
     }else if (laundry % 5){
